1774ace74e2315c1850ad9db1830883e47a66c64
Nilesh <nileshv@askinfosys.com>
2022-06-14 12:20:08 +0530

    Updates

---
+++ precache-manifest.61742792bc9216aef9ef3950342a17e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e04a6740e01024f71cb",
+    "url": "/genxiotapp/static/css/main.94bc799c.chunk.css"
+  },
+  {
+    "revision": "6e04a6740e01024f71cb",
+    "url": "/genxiotapp/static/js/main.6e04a674.chunk.js"
+  },
+  {
+    "revision": "e67e65479d4f06baac99",
+    "url": "/genxiotapp/static/js/1.e67e6547.chunk.js"
+  },
+  {
+    "revision": "557ff4a1cf80793accdd",
+    "url": "/genxiotapp/static/js/2.557ff4a1.chunk.js"
+  },
+  {
+    "revision": "73db9769199d07601808",
+    "url": "/genxiotapp/static/js/runtime~main.73db9769.js"
+  },
+  {
+    "revision": "123b8ca911d4f4df9bc34ed1b67a9020",
+    "url": "/genxiotapp/static/media/deviceimage.123b8ca9.png"
+  },
+  {
+    "revision": "ab8d6c0b8e2c9c4e9a8fbb62a9ac8f3a",
+    "url": "/genxiotapp/index.html"
+  }
+];
